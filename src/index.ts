14563bbac123c9cb3e0e7b6de2695e8378675c46
Artem Ryazanov <artryazanov@gmail.com>
2025-12-19 23:16:53 +0700

    chore: bump version to 1.0.1 in package.json and CLI.

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name('esrb-gen')
   .description('Generates ESRB Rating Slates based on game title and platform')
-  .version('1.0.0')
+  .version('1.0.1')
   .option('-g, --game <title>', 'Game title')
   .option('-u, --url <url>', 'ESRB game URL')
   .option('-r, --rating <category>', 'Rating category (e.g., E, T, M)')
