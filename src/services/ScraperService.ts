8c386d4e65c5cf9009890376d4694407abbb3f60
Artem Ryazanov <artryazanov@gmail.com>
2025-12-22 23:51:30 +0700

    Add Connection: 'keep-alive'

--- src/services/ScraperService.ts
+++ src/services/ScraperService.ts
@@ -43,7 +43,6 @@ export class ScraperService {
             'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
           'Accept-Language': 'en-US,en;q=0.9',
           Referer: 'https://www.esrb.org/',
-          Connection: 'keep-alive',
         },
       });
 
@@ -212,7 +211,6 @@ export class ScraperService {
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Language': 'en-US,en;q=0.9',
         Referer: 'https://www.esrb.org/',
-        Connection: 'keep-alive',
       },
     });
 
